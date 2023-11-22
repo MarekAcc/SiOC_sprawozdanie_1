904f72f0d4ad5c7556f3b99ec7659f917b3ffa10
Marek Wasiewicz <marekwasiewicz@MacBook-Air-Marek.local>
2023-11-22 16:41:58 +0100

    interpolacja 3 stopnia

--- main.py
+++ main.py
@@ -63,22 +63,60 @@ def quadratic_interpolation(x1, x2, y1):
                 if x2[j] >= x1[i]:
                     tmp = factors[0]*x2[j]**2 + factors[1]*x2[j] + factors[2]
                     y2.append(tmp)
-            plt.plot(x2, y2, '.b')
-            plt.show()
-            return
+            return y2
         if (i + 2) >= len(x1):
             factors = find_quadratic_factors(x1[i-1], x1[i], x1[i+1],y1[i-1], y1[i], y1[i+1])
             for j in range(len(x2)):
                 if x2[j] >= x1[i]:
                     tmp = factors[0]*x2[j]**2 + factors[1]*x2[j] + factors[2]
                     y2.append(tmp)
-            plt.plot(x2, y2, '.b')
-            plt.show()
-            return
+            return y2
 
         else:
             factors = find_quadratic_factors(x1[i], x1[i+1], x1[i+2],y1[i], y1[i+1], y1[i+2])
             for j in range(len(x2)):
                 if x2[j] >= x1[i] and x2[j] < x1[i+2]:
                     tmp = factors[0]*x2[j]**2 + factors[1]*x2[j] + factors[2]
+                    y2.append(tmp)
+
+# Interpolacja trzeciego stopnia
+def find_cubic_factors(x1,x2,x3,x4,y1,y2,y3,y4):
+    y = [y1,y2,y3,y4]
+    mat = [[x1**3,x1**2,x1,1],[x2**3,x2**2,x2,1],[x3**3,x3**2,x3,1],[x4**3,x4**2,x4,1]]
+    # Odwrotność macierzy
+    mat = np.linalg.inv(mat)
+    # Odwrotność razy dane punkty
+    wsp = mat @ y
+    return wsp
+def cubic_interpolation(x1,x2,y1):
+    y2=[]
+    for i in range(0, len(x1), 3):
+        if (i + 1) >= len(x1):
+            factors = find_cubic_factors(x1[i-3],x1[i-2], x1[i-1], x1[i],y1[i-3], y1[i-2], y1[i-1], y1[i])
+            for j in range(len(x2)):
+                if x2[j] >= x1[i]:
+                    tmp = factors[0]*x2[j]**3 + factors[1]*x2[j]**2 + factors[2]*x2[j] + factors[3]
+                    y2.append(tmp)
+            return y2
+
+        if (i + 2) >= len(x1):
+            factors = find_cubic_factors(x1[i-2], x1[i-1], x1[i], x1[i+1], y1[i-2], y1[i-1], y1[i], y1[i+1])
+            for j in range(len(x2)):
+                if x2[j] >= x1[i]:
+                    tmp = factors[0]*x2[j]**3 + factors[1]*x2[j]**2 + factors[2]*x2[j] + factors[3]
+                    y2.append(tmp)
+            return y2
+
+        if (i + 3) >= len(x1):
+            factors = find_cubic_factors(x1[i-1], x1[i], x1[i+1], x1[i+2], y1[i-1], y1[i], y1[i+1], y1[i+2])
+            for j in range(len(x2)):
+                if x2[j] >= x1[i]:
+                    tmp = factors[0]*x2[j]**3 + factors[1]*x2[j]**2 + factors[2]*x2[j] + factors[3]
+                    y2.append(tmp)
+            return y2
+        else:
+            factors = find_cubic_factors(x1[i], x1[i+1], x1[i+2], x1[i+3], y1[i], y1[i+1], y1[i+2], y1[i+3])
+            for j in range(len(x2)):
+                if x2[j] >= x1[i] and x2[j] < x1[i+3]:
+                    tmp = factors[0]*x2[j]**3 + factors[1]*x2[j]**2 + factors[2]*x2[j] + factors[3]
                     y2.append(tmp)
